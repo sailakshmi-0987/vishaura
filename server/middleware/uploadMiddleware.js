af1d83edad3d72d13e520feaaa22d18906d629c7
sailakshmi-0987 <gajulapallisailakshmi@gmail.com>
2026-03-24 21:45:34 +0530

    allowed 15mb uploads

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -3,7 +3,10 @@ import multer from "multer";
 const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage: storage
+  storage: storage,
+  limits:{
+    fileSize:20*1024*1024
+  }
 });
 
 export default upload;
